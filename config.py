d81fb3b0a1214d6192bc2de4f7f2a3dbef53119d
Admin <RitaSot8@yandex.ru>
2025-01-21 17:20:33 +0300

    +border

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
-dragons_screen_width, dragons_screen_height = 800, 800
-FPS = 15
-size_of_field = 20
+dragons_screen_width, dragons_screen_height = 700, 700
+FPS = 5
+size_of_field = 50
+border_thickness = 100
